394c17fb5194c41a10688cdd074205e51c4905e8
Alexandre Rivaux <alex@bonjour-lab.com>
2016-07-10 17:46:27 +0200

    20160710 - ofx

--- VideoSynth_LearningOFX/src/main.cpp
+++ VideoSynth_LearningOFX/src/main.cpp
@@ -5,12 +5,9 @@
 int main( ){
 	ofSetupOpenGL(1280,720,OF_WINDOW);			// <-------- setup the GL context
 	/*ofGLWindowSettings settings;
-	settings.width = 1280;
-	settings.height = 720;
-	settings.setGLVersion(4, 5); /// < select your GL Version here
-	settings.windowMode = OF_WINDOW;
-	ofCreateWindow(settings); ///< create your window here
-	*/
+	settings.setGLVersion(3, 2);
+	ofCreateWindow(settings);*/
+
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
